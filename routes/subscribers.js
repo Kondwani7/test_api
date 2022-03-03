910da2a55bffd3dd192a3125c23326d8f39ed459
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-03-03 13:31:47 +0200

    post request

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const router = express.Router()
-const Subcriber = require('../models/subcriber');
+const app = express()
+const router = express.Router();
+const Subscriber = require('../models/subscriber');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json)
+const jsonParser = bodyParser.json()
+
 
 
 //get all subscribers
 router.get('/', async (req, res) => {
     try{
-        const subscribers = await Subcriber.find()
+        const subscribers = await Subscriber.find()
         res.json(subscribers)
     }catch(err){
         res.status(500).json({message: err.message});
@@ -17,9 +23,19 @@ router.get('/:id', (req, res) => {
     res.send(req.params.id)
 })
 //create a subscriber
-router.post('/', (req, res)  => {
-    res.send()
+router.post('/', async(req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        channel: req.body.channel
+    });
+    try{
+        const newSubscriber = await this.post.save();
+        res.json(newSubscriber);
+    }catch(err){
+        res.json({message:err.message})
+    }
 })
+
 //update subscriber based on a specific field of interest
 router.patch('', (req,res)=> {
 
